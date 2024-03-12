88737a50709a9da9bcee25fd2f0a4c167d19c9b8
nanorisrael <israel.nanor@compozelabs.com>
2024-03-12 19:49:23 +0000

    Update home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { io, Socket } from "socket.io-client";
 import Home from './pages/home';
 
+const socket = io('http://localhost:4000');
+
 function App() {
+  const [username, setUsername] = useState<string>('');
+  const [room, setRoom] = useState<string>('');
+
   return (
     <Router>
       <div className='App'>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route
+            path='/'
+            element={
+              <Home
+                username={username}
+                setUsername={setUsername} // Add this
+                room={room} // Add this
+                setRoom={setRoom} // Add this
+                socket={socket} // Add this
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
   );
 }
-
 export default App;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,47 @@
 import styles from './styles.module.css';
+import { Socket } from 'socket.io-client'
+import { useNavigate } from 'react-router-dom'; // Add this
 
-const Home = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.formContainer}>
-        <h1>{`<>DevRooms</>`}</h1>
-        <input className={styles.input} placeholder='Username...' />
 
-        <select className={styles.input}>
-          <option>-- Select Room --</option>
-          <option value='javascript'>JavaScript</option>
-          <option value='node'>Node</option>
-          <option value='express'>Express</option>
-          <option value='react'>React</option>
-        </select>
+type HomeProps = {
+	username: string
+	setUsername: (name: string) => void
+	room: string
+	setRoom: (room: string) => void
+	socket: Socket
+}
+function Home({ username, setUsername, room, setRoom, socket }: HomeProps) {
+	const navigate = useNavigate(); // Add this
 
-        <button className='btn btn-secondary'>Join Room</button>
-      </div>
-    </div>
-  );
+	const joinRoom = () => {
+		if (room !== '' && username !== '') {
+		  socket.emit('join_room', { username, room });
+		}
+
+		navigate('/chat', { replace: true }); // Add this
+	};
+	return (
+		<div className={styles.container}>
+		<input
+			className={styles.input}
+			placeholder='Username...'
+			onChange={(e) => setUsername(e.target.value)} // Add this
+		/>
+
+		<select
+			className={styles.input}
+			onChange={(e) => setRoom(e.target.value)} // Add this
+		>
+		</select>
+		<button
+          className='btn btn-secondary'
+          style={{ width: '100%' }}
+          onClick={joinRoom} // Add this
+        >
+          Join Room
+        </button>
+		</div>
+	);
 };
 
 export default Home;
